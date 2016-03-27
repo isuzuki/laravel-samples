f1d532ca0c8c0410b65ee9c2cd4855360de95d49
isuzuki <isuzuki@suzukiikuonoAir.lan>
2016-03-28 04:21:10 +0900

    add laravel pjax sample

---
+++ resources/views/pjax/about.blade.php
@@ -0,0 +1,10 @@
+@extends('pjax.base')
+
+@section('content')
+<main id="main">
+  <h1>pjax about</h1>
+  <div>
+    <p>pjax about page</p>
+  </div>
+</main>
+@stop
---
+++ resources/views/pjax/index.blade.php
@@ -0,0 +1,10 @@
+@extends('pjax.base')
+
+@section('content')
+<main id="main">
+  <h1>pjax index</h1>
+  <div>
+    <p>pjax index page</p>
+  </div>
+</main>
+@stop
