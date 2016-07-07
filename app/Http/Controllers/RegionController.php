2bbcec2b43cd93d191dfac07aceda35be1a5e8c5
isuzuki <isuzuki@suzukiikuo-no-MacBook-Air.local>
2016-07-08 02:56:58 +0900

    add n+1 and eager loading

---
+++ app/Http/Controllers/RegionController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Region;
+use Illuminate\Http\Request;
+
+class RegionController extends Controller
+{
+    protected $region;
+
+    public function __construct(Region $region)
+    {
+        $this->region = $region;
+    }
+
+    public function prefectureN1()
+    {
+        $regions = $this->region->get();
+        return view('region.prefecture', compact('regions'));
+    }
+
+    public function prefectureEager()
+    {
+        $regions = $this->region->with('prefectures')->get();
+        return view('region.prefecture', compact('regions'));
+    }
+
+    public function cityN1()
+    {
+        $regions = $this->region->get();
+        return view('region.city', compact('regions'));
+    }
+
+    public function cityEager()
+    {
+        $regions = $this->region->with('cities')->get();
+        return view('region.city', compact('regions'));
+    }
+}
